a41c7a312e0ae0429e20b80360efd4b5387b23c3
Devin Criswell <devin@shoebacca.com>
2019-04-25 20:02:44 -0500

    update 2 Mod 3

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -35,18 +35,22 @@ namespace GradeBook.GradeBooks
 
         public override void CalculateStatistics()
         {
-            if(Students.Count < 5)
+            if (Students.Count < 5)
+            {
                 Console.WriteLine("Ranked grading requires at least 5 students.");
                 return;
+            }
 
             base.CalculateStatistics();
         }
 
         public override void CalculateStudentStatistics(string name)
         {
-            if(Students.Count < 5)
+            if (Students.Count < 5)
+            {
                 Console.WriteLine("Ranked Grading requires at least 5 students.");
                 return;
+            }
             base.CalculateStudentStatistics(name);
         }
     }   
