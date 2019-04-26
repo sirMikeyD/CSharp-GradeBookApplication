ffc3a0b7a456a7af17f53211190a6e6fae57ed10
Devin Criswell <devin@shoebacca.com>
2019-04-25 19:59:29 -0500

    module 3

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -31,9 +31,24 @@ namespace GradeBook.GradeBooks
                 return 'D';
             else
                 return 'F';
-
-
-            
         }
-    }
+
+        public override void CalculateStatistics()
+        {
+            if(Students.Count < 5)
+                Console.WriteLine("Ranked grading requires at least 5 students.");
+            return;
+
+            base.CalculateStatistics();
+        }
+
+        public override void CalculateStudentStatistics(string name)
+        {
+            if(Students.Count < 5)
+                Console.WriteLine("Ranked Grading requires at least 5 students.");
+            return;
+            base.CalculateStudentStatistics(name);
+        }
+    }   
+        
 }
