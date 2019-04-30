a40789184f18e62cede47f52edff8c2487879092
Devin Criswell <devin@shoebacca.com>
2019-04-30 16:00:54 -0500

    mosd 5 update 77

--- GradeBook/UserInterfaces/StartingUserInterface.cs
+++ GradeBook/UserInterfaces/StartingUserInterface.cs
@@ -54,7 +54,7 @@ namespace GradeBook.UserInterfaces
             }
 
             Console.WriteLine("Created gradebook {0}.", name);
-           //GradeBookUserInterface.CommandLoop(gradeBook);
+            GradeBookUserInterface.CommandLoop(gradeBook);
         }
 
         public static void LoadCommand(string command)
